c3145b8f7f42fcd0e816cbbee7c876b1e2915944
alanc <alanc@alans-mbp.home>
2016-07-24 16:42:43 -0400

    c

--- src/com/java_data_structure/two_d_hourglass.java
+++ src/com/java_data_structure/two_d_hourglass.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class two_d_array {
+public class two_d_hourglass {
 	
 
 	public static void main(String[] args) {
