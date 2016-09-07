77ae5f9581627dbc3fdc8031e11e7e86c997b7b3
alanc <alanc@alans-mbp.home>
2016-09-06 20:31:35 -0400

    c

--- src/com/algorithms_warmup/staircase.java
+++ src/com/algorithms_warmup/staircase.java
@@ -8,6 +8,7 @@ public class staircase {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter ");
         int num = sc.nextInt();
         for (int i=0; i < num; i++){
             for (int j=0; j < num; j++){
